47b17ff34cb594c2805d7df7480c7554f9238a5d
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-11-16 15:37:22 +0530

    Implements digit-frequency function

--- digit-frequency.c
+++ digit-frequency.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+
+int* findDigitFrequency(char string[]) {
+    static int digitFrequency[10] = {0,0,0,0,0,0,0,0,0,0};
+
+    int counter = 0;
+
+    while (string[counter] != '\0') {
+        int currentDigit = string[counter] - 48;
+
+        digitFrequency[currentDigit] += 1;
+        counter = counter + 1;
+    }
+
+    return digitFrequency;
+}
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char string[1000];
+    scanf("%s", string);
+
+    int *digitFrequency = findDigitFrequency(string);
+
+    for (int index = 0; index < 10; index++) {
+        printf("%d ", digitFrequency[index]);
+    }
     
     return 0;
 }
